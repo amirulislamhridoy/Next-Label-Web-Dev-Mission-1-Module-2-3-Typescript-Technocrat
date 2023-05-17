7805dd123d958ef3f7fc39d35ca7e3b2329610e3
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-05-17 23:58:13 +0600

    aa

--- src/module-3/asynchronous.ts
+++ src/module-3/asynchronous.ts
@@ -1,1 +1,26 @@
-// asynchoronous 
+// asynchoronous typescript
+
+function checkAsyncronous(): Promise<string> {
+    return new Promise<string>((resolve, rejected) => {
+        const data = 'hello sir';
+        if (data) {
+            resolve('yes')
+        } else {
+            rejected('no')
+        }
+    })
+}
+const checkAsyncronous2 = async () => {
+    return await checkAsyncronous()
+}
+checkAsyncronous2()
+
+const getTodo = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await res.json()
+    return data
+}
+const getTodoData = async (): Promise<void> => {
+    await getTodo()
+}
+getTodoData()
