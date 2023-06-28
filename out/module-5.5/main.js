c7c46f9fc69c9e7d42857a872326a62f5e62edcc
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-06-28 12:31:55 +0600

    import & export

--- out/module-5.5/main.js
+++ out/module-5.5/main.js
@@ -1,3 +1,20 @@
-export const add5 = (num1, num2) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.multiple = exports.subtract = exports.add5 = void 0;
+const add5 = (num1, num2) => {
     return num1 + num2;
 };
+exports.add5 = add5;
+const subtract = (num1, num2) => {
+    return num1 - num2;
+};
+exports.subtract = subtract;
+const multiple = (num1, num2) => {
+    return num1 * num2;
+};
+exports.multiple = multiple;
+const divide = (num1, num2) => {
+    return num1 / num2;
+};
+exports.default = divide;
+// export default {subtract,multiple,divide}
--- out/module-5.5/module.js
+++ out/module-5.5/module.js
@@ -1,6 +1,9 @@
-import { add5 as added } from './main';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// import * as main from './main';
+// import divid, {add5 as added} from './main';
 const add5 = (num1, num2, num3) => {
     return num1 + num2 + num3;
 };
-added(2, 3);
+// added(2,3)
 add5(2, 3, 4);
