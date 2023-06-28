fa537a02d3dc4fa1a48efdb33e6248ba4d025a35
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-06-28 12:31:05 +0600

    multiple export & import

--- src/module-5.5/main.ts
+++ src/module-5.5/main.ts
@@ -1,3 +1,18 @@
 export const add5 = (num1:number, num2:number):number => {
     return num1+num2
 }
+
+const subtract = (num1:number, num2:number):number => {
+    return num1-num2
+}
+const multiple = (num1:number,num2:number):number => {
+    return num1*num2
+}
+const divide =(num1:number,num2:number):number => {
+    return num1/num2
+}
+
+export {subtract, multiple}
+export default divide;
+
+// export default {subtract,multiple,divide}
--- src/module-5.5/module.ts
+++ src/module-5.5/module.ts
@@ -1,8 +1,10 @@
-import {add5 as added} from './main';
+import divid, {add5 as added} from './main';
+// import * as main from './main';
+// import divid, {add5 as added} from './main';
 
 const add5 = (num1:number, num2:number, num3:number):number => {
     return num1+num2+num3
 }
 
-added(2,3)
+// added(2,3)
 add5(2,3,4)
